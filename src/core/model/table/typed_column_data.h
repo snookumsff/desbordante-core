2c2ee787adaa9dfe1cdd887faf20658b98023554
snookumsff <annaltsh@mail.ru>
2026-04-28 14:30:01 +0000

    Fix reliance on unordered_map iteration order in type detection
    The type detection previously iterated through unordered_map, which does
    not have order guarantees. This caused the TestTypeParsing tests to fail
    when a new type was added because numbers that were detected as ints
    before started being detected as doubles.
    
    This replaces unordered_map with vector and reorders entries to ensure
    kDate -> kInt -> kBigInt -> kDouble -> kString order.

--- src/core/model/table/typed_column_data.h
+++ src/core/model/table/typed_column_data.h
@@ -216,23 +216,23 @@ private:
                 }
                 return is_simple_date;
             };
-    inline static std::unordered_map<TypeId, std::function<bool(std::string const&)>> const
+    inline static std::vector<std::pair<TypeId, std::function<bool(std::string const&)>>> const
             kTypeIdToChecker = {
-                    {TypeId::kDouble,
+                    {TypeId::kDate,
                      [](std::string const& val) {
-                         return boost::regex_match(val, kTypeIdToRegex.at(TypeId::kDouble));
-                     }},
-                    {TypeId::kBigInt,
-                     [](std::string const& val) {
-                         return boost::regex_match(val, kTypeIdToRegex.at(TypeId::kBigInt));
+                         return boost::regex_match(val, kTypeIdToRegex.at(TypeId::kDate)) &&
+                                (kDelimitedDateCheck(val) || kUndelimitedDateCheck(val));
                      }},
                     {TypeId::kInt,
                      [](std::string const& val) {
                          return boost::regex_match(val, kTypeIdToRegex.at(TypeId::kInt));
                      }},
-                    {TypeId::kDate, [](std::string const& val) {
-                         return boost::regex_match(val, kTypeIdToRegex.at(TypeId::kDate)) &&
-                                (kDelimitedDateCheck(val) || kUndelimitedDateCheck(val));
+                    {TypeId::kBigInt,
+                     [](std::string const& val) {
+                         return boost::regex_match(val, kTypeIdToRegex.at(TypeId::kBigInt));
+                     }},
+                    {TypeId::kDouble, [](std::string const& val) {
+                         return boost::regex_match(val, kTypeIdToRegex.at(TypeId::kDouble));
                      }}};
     // each 1 represents a possible type from kAllCandidateTypes
     inline static std::unordered_map<TypeId, std::bitset<5>> const kTypeIdToBitset = {
